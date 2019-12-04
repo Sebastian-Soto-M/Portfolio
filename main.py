23583c3910abd739403f71991ba3907e66de20a6
Sebastian Noe Soto Madrigal <dev.snsm@gmail.com>
2019-12-04 16:36:04 -0600

    technologies with indicator and manual use

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def inject_personal_info():
 @app.route('/')
 def index():
     techs_json = utils.readJson(os.path.join('content', 'technologies.json'))
-    return render_template('views/index/index.html', technologies=sorted(techs_json, key=itemgetter('name'),reverse=False))
+    return render_template('views/index/index.html', technologies=techs_json)
 
 
 @app.errorhandler(404)
